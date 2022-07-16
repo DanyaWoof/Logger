dfc932782247de4c5c89d9717085293d18abfd49
DanyaAnalProgger <smartden777@gmail.com>
2022-07-16 19:15:40 +0300

    init commit

---
+++ scrypt.js
@@ -0,0 +1,14 @@
+
+table {
+    border-collapse: collapse;
+    /*убираем пустые промежутки между ячейками*/
+    border: 0px solid grey;
+    /*устанавливаем для таблицы внешнюю границу серого цвета толщиной 1px*/
+    width: 50%;
+    }
+
+th, td {
+    border: 1px solid grey;
+    text-align: center;
+    align-content: center;
+    }
